f8c64f4a7a82b4573efca648d1dfafeca6e80e9c
Hebert Silva <68448845+SilvasHebert@users.noreply.github.com>
2024-12-15 23:36:43 -0300

    Update tokens.ts

--- src/consts/tokens.ts
+++ src/consts/tokens.ts
@@ -1,3 +1,3 @@
-export const GoogleClientId = '397669612150-kkc5rvd5pqn1vn11k8pt3qaujl33ncai.apps.googleusercontent.com';
+export const GoogleClientId = '';
 
-export const RealmAppId = 'dysrupveiculos-jywvv';
+export const RealmAppId = '';
